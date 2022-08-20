f6722d1146efe5ed473670fb3f8ba541076ff337
Hillary Ogieleguea <hillariosky123@gmail.com>
2022-08-20 14:04:38 +0100

    Added sitemaps for seo

--- HiloBlog/settings.py
+++ HiloBlog/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
     'blog',
     'sorl.thumbnail',
     'crispy_forms',
